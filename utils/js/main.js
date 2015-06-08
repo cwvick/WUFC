2ef82634701015744a20cb8b30d078da64a7b8f6
vchuang <vchuang@digitalriver.com>
2015-06-08 16:02:02 +0800

    Flashlights and mobile version

--- utils/js/main.js
+++ utils/js/main.js
@@ -28,6 +28,7 @@ $(function() {
     var menubox_width = $('.menubox').outerWidth();
     if ( action == 'close' ) {
       $('.wrap').animate({
+        
         left: -menubox_width + 'px'
       }, 500, function() {
         $('.btn_arrow').removeClass('close').addClass('open');
@@ -43,7 +44,7 @@ $(function() {
   };
 
   var playerAnimation = function() {
-    var $palyerImg = $('.section_01 div[class^="player_"] img')
+    var $palyerImg = $('.section_01 div[class^="player_"] img');
     var img_amount  = $palyerImg.length;
     var count = 0;
 
@@ -68,27 +69,89 @@ $(function() {
     }    
   };
 
+  var setFlashlight = function() {
+    var lightsVariation = 3;
+    var lightsAmount = 220;
+    var content = '';
+    var minTop = 432, maxTop = 643, minLeft = 5, maxLeft = $(window).width(), minTop2 = 320, maxTop2 = 460, minLeft2 = 260;
+
+    for (i = 0; i < lightsAmount; i++) {
+      var lightNum = Math.floor(Math.random()*lightsVariation) + 1;
+      var lightTop = Math.random() * (maxTop - minTop) + minTop;
+      var lightLeft = Math.random() * (maxLeft - minLeft) + minLeft;
+
+      if ( i % 2 && ( i > 20 || i < 200 ) ) {
+        lightTop = Math.random() * (maxTop2 - minTop2) + minTop2;
+        lightLeft = Math.random() * (maxLeft - minLeft2) + minLeft2;
+      }
+      content += '<div class="flashlight flashlight_0' + lightNum + '" style="top: ' + lightTop + 'px; left: ' + lightLeft + 'px;"></div>';
+    }
+
+    $('.section_01 .bg').append(content);
+    lightsAnimation(lightsAmount, 0);
+  };
+
+  var lightsAnimation = function(amount, lightNum) {
+    if ( lightNum < amount ) {
+      $('.section_01 .bg .flashlight').eq(lightNum).fadeIn('fast', function() {
+        lightsAnimation(amount, lightNum+1);
+        
+        setTimeout(function() {
+          $('.section_01 .bg .flashlight').eq(lightNum).hide();
+        }, 1000);
+      });
+    } else {
+      lightsAnimation(amount, 0);
+    }
+  };
+
+  var setPageScroll = function() {
+    $('.main').onepage_scroll({
+      sectionContainer: 'div[class^="section_"]',
+      pagination: false,
+      afterMove: function(index) {
+        if ( index == 1 ) {
+          $('.btn_backTop').hide();
+        } else {
+          $('.btn_backTop').show();
+          sideMenuHandler('open');
+        }
+        menuArrowHandler(index);
+      }
+    });
+  };
+
   var initUI = function() {
     sideMenuHandler('close');
-    playerAnimation();
+    if ( $(window).width() < 800) {
+      // 
+    } else {
+      console.log( $(window).width());
+      playerAnimation();
+      setFlashlight();
+      setPageScroll();
+    }
   };
 
   initUI();
 
-  $('.main').onepage_scroll({
-    sectionContainer: 'div[class^="section_"]',
-    pagination: false,
-    afterMove: function(index) {
-      if ( index == 1 ) {
-        $('.btn_backTop').hide();
-      } else {
-        $('.btn_backTop').show();
-      }
-      menuArrowHandler(index);
+  $(window).load(function() {
+    $('body, html').scrollTop(0);
+
+    if ( $(window).width() >= 800 && !$('.section_01 .player_01').is(':visible') && !$('.section_01 .player_02').is(':visible') && !$('.section_01 .player_03').is(':visible') ) {
+      $('.section_01 .player_03').fadeIn('slow', function() {
+        $('.section_01 .player_02').fadeIn('slow', function() {
+          $('.section_01 .player_01').fadeIn('slow');
+        });
+      });
     }
   });
 
-  $(window).load(function() {
-    $('body, html').scrollTop(0);
+  $(window).resize(function() {
+    var winWidth = $(window).width();
+
+    if ( (winWidth < 800 && $('.onepage-wrapper').length > 0) || winWidth >= 800 && $('.onepage-wrapper').length == 0 ) {
+      location.reload();
+    }
   });
 });
